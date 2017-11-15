3999184b1ba5273425364e86f7d842e9c3da2087
dicotha <wbn_k@hotmail.com>
2017-11-15 21:27:06 -0200

    first commit

---
+++ App2/App2/InfoContatoPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class InfoContatoPage : ContentPage
+	{
+		public InfoContatoPage (Contato contato)
+		{
+			InitializeComponent ();
+
+            this.Title = contato.Nome;
+            var telefoneint = contato.Telefone;
+            
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MaisDetalhes());
+        }
+    }
+}
---
+++ App2/App2/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2
+{
+    public class Contato    {
+        public string Nome { get; set; }
+        public decimal Telefone { get; set; }
+    }
+
+
+
+    public partial class MainPage : ContentPage
+	{
+        public List<Contato> Contatos { get; set; }
+
+        public MainPage()
+		{
+			InitializeComponent();
+            this.Contatos = new List<Contato>
+            {
+                new Contato {Nome = "Waldir", Telefone = 946250052 },
+                new Contato {Nome = "Thalita", Telefone = 946250052 },
+                new Contato {Nome = "Bernardo", Telefone = 946250052 },
+            };
+            listaContatos.ItemsSource = this.Contatos;
+            this.BindingContext = this;
+
+        }
+
+        private void listaContatos_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var telefone = (Contato)e.Item;
+
+            Navigation.PushAsync(new InfoContatoPage(telefone));
+        }
+    }
+}
